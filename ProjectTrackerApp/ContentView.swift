1398ef3f62fd901bf5f7f73816b97f882da1ca98
Andy Liang <yf98.liang@gmail.com>
2024-11-13 15:40:31 -0800

    Initial Commit

---
+++ ProjectTrackerApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ProjectTrackerApp
+//
+//  Created by Andy Liang on 2024-11-13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ProjectTrackerApp/ProjectTrackerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectTrackerAppApp.swift
+//  ProjectTrackerApp
+//
+//  Created by Andy Liang on 2024-11-13.
+//
+
+import SwiftUI
+
+@main
+struct ProjectTrackerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
