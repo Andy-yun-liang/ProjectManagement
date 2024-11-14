689728089d8671324eb4410130ed6f57230db63c
Andy Liang <yf98.liang@gmail.com>
2024-11-13 16:15:50 -0800

    -setup swift data model -renamed files both contentview and projecttrackerappapp

---
+++ ProjectTrackerApp/Models/ProjectModel.swift
@@ -0,0 +1,53 @@
+//
+//  ProjectModel.swift
+//  ProjectTrackerApp
+//
+//  Created by Andy Liang on 2024-11-13.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+class Project: Identifiable {
+    
+    @Attribute(.unique) var id: String
+    var startDate = Date()
+    var name = ""
+    var focus = ""
+    @Relationship(deleteRule:.cascade,inverse: \ProjectUpdate.project)
+    var update: [ProjectUpdate] = [ProjectUpdate()]
+    
+    init(){
+        id = UUID().uuidString
+    }
+    
+    
+}
+
+
+@Model
+class ProjectUpdate: Identifiable {
+    
+    @Attribute(.unique) var id : String
+    var headline = ""
+    var summary = ""
+    var date = Date()
+    var hours = 0.0
+    var updateType = UpdateType.logs
+    var project: Project?
+    
+    init(){
+        id = UUID().uuidString
+    }
+    
+    
+}
+
+
+enum UpdateType: Codable {
+    
+    case logs
+    case milestone
+    
+}
